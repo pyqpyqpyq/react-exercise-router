3e2d0daee0c9ff639855b0a361615b99e420e9d2
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-10-06 11:43:08 +0800

    [pyq]feat:implement aboutus page and app router

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,13 +1,35 @@
-import React, {Component} from 'react';
-import '../styles/App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import React, { Component } from "react";
+import "../styles/App.css";
+import {
+  BrowserRouter,
+  BrowserRouter as Router,
+  Link,
+  Switch,
+} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Router />
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <div className="header">
+            <div className="headerInfo">
+              <Link to="/aboutus">About us</Link>
+            </div>
+            <div className="headerInfo">
+              <Link to="/myprofile">My profile</Link>
+            </div>
+            <div className="headerInfo">
+              <Link to="/">Home</Link>
+            </div>
+          </div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/myprofile" component={MyProfile} />
+            <Route exact path="/aboutus" component={AboutUs} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/exercise-1/components/aboutus.js
@@ -0,0 +1,16 @@
+import React,{Component}from 'react';
+import {Link}from 'react-router-dom';
+import'../styles/App.css';
+
+export default class aboutus extends Component{
+    render(){
+        return(
+            <div className='aboutUs'>
+                <p>Company: Thoughtworks Local</p>     
+                <p>Location: Xian</p>  
+                <p className='secondp'>For more information, please</p>     
+                <p>view our <label className="website"><Link to='/'>website</Link></label></p>                                      
+            </div>
+        )
+    }
+}
